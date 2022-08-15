171f904ef7f13c43683100676615110cb5990dca
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-08-15 21:12:07 +0900

    [Finish]4章

---
+++ Chapter04.java
@@ -0,0 +1,15 @@
+public class Chapter04 {
+
+  public static void main(String[] args) {
+
+    String name = "Chikuwa";
+    System.out.println(name);
+
+    name = "sleep";
+    System.out.println(name);
+
+    final int LUCKY_NUMBER = 7;
+    System.out.println(LUCKY_NUMBER);
+
+  }
+}
